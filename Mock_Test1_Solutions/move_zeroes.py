13c702179f0133b837bffa8323343ca38fc9101c
arpmit-a <112280277+arpmit-a@users.noreply.github.com>
2023-06-25 11:37:20 +0530

    Update move_zeroes.py

--- Mock_Test1_Solutions/move_zeroes.py
+++ Mock_Test1_Solutions/move_zeroes.py
@@ -1,8 +1,13 @@
-index=0
-for i in range(0,len(nums)):
-  if(nums[i]!=0):
-    nums[index]=nums[i]
-    index+=1
-while(index<len(nums)):
-  nums[index]=0
-  index+=1
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        index=0
+        for i in range(0,len(nums)):
+            if(nums[i]!=0):
+                nums[index]=nums[i]
+                index+=1
+        while(index<len(nums)):
+            nums[index]=0
+            index+=1
